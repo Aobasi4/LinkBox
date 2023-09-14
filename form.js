a41c5c9e2fcaad08e628863e666cfddcf573bf16
Amara Obasi <aobasi4@gmail.com>
2023-09-14 12:24:34 -0500

    boilerplate form

---
+++ form.js
@@ -0,0 +1,11 @@
+<form>
+    
+<label for="basic-url" class="form-label">Insert URL</label>
+    <div class = "mb-3">
+    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"/>
+    </div>
+
+    <div class = "mb-3">
+        <label> Link Name </label>
+    </div>
+</form>

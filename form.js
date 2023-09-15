df4723f93302b3cdcce214240247bb23523276c6
Amara Obasi <aobasi4@gmail.com>
2023-09-15 17:42:22 -0500

    Something but not really anything

--- form.js
+++ form.js
@@ -1,11 +1,14 @@
 <form>
-    
+
 <label for="basic-url" class="form-label">Insert URL</label>
     <div class = "mb-3">
     <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"/>
     </div>
 
     <div class = "mb-3">
-        <label> Link Name </label>
+        <label for = "link-name" class = "form-label"> Insert name for Link </label>
+        <input type="text" class="form-control" id="link-name" aria-describedby="basic-addon3"/>
     </div>
+
+    <button class="btn btn-primary" type = "submit"> Create Link </button>
 </form>
